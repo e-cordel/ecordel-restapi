3546e933a89cdf71d4cd7696243ad6ede28e1caa
Mário Santos <super.mario.santos.sousa@gmail.com>
2020-04-10 17:46:20 +0100

    Add test clas for CordelController

---
+++ src/test/java/br/com/itsmemario/ecordel/cordel/CordelControllerTest.java
@@ -0,0 +1,35 @@
+package br.com.itsmemario.ecordel.cordel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {CordelController.class}, secure = false)
+public class CordelControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CordelService cordelService;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getCordel() throws Exception {
+        mockMvc.perform(get("/cordels/1"))
+                .andExpect(status().isOk());
+    }
+}
