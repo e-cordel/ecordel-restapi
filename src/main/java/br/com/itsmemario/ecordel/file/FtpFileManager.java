757e7fed5a6c1647e8f6835637d61ac564a0d007
Karlos Kelvin <karloskelvinsantos@gmail.com>
2021-02-28 10:57:56 -0300

    Implemented FtpFileManager to working FTP server

--- src/main/java/br/com/itsmemario/ecordel/file/FtpFileManager.java
+++ src/main/java/br/com/itsmemario/ecordel/file/FtpFileManager.java
@@ -17,24 +17,70 @@
 
 package br.com.itsmemario.ecordel.file;
 
+import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
-public class FtpFileManager implements FileManager{
+public class FtpFileManager implements FileManager {
 
-    private FTPClient ftpClient;
+    @Value("${ftp.server}")
+    private String server;
+
+    @Value("${fpt.port}")
+    private int port;
+
+    @Value("${fpt.user}")
+    private String user;
+
+    @Value("${fpt.password}")
+    private String password;
+
+    @Value("${ftp.directory}")
+    private String directory;
+
+    private final FTPClient ftpClient;
 
     public FtpFileManager(FTPClient ftpClient) {
         this.ftpClient = ftpClient;
     }
 
-    @Override
-    public String saveFile(byte[] bytes, String fileName) {
+    void connectServer() throws IOException {
+        ftpClient.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+
+        ftpClient.connect(server, port);
+
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.changeWorkingDirectory(directory);
+
+        int reply = ftpClient.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftpClient.disconnect();
+            throw new IOException("Exception in connecting to FTP server");
+        }
+
+        ftpClient.login(user, password);
+    }
+
+    void closeServer(){
         try {
+            ftpClient.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public String saveFile(byte[] bytes, String fileName){
+        try {
+            connectServer();
+
             ftpClient.storeFile(fileName, new ByteArrayInputStream(bytes));
+
+            closeServer();
             return fileName;
         } catch (IOException e) {
             throw new FileProcessException("Error when saving file", e);
@@ -43,11 +89,30 @@ public class FtpFileManager implements FileManager{
 
     @Override
     public InputStream getFile(String fileName) {
-        return null;
+       try {
+           connectServer();
+
+           InputStream inputStream = ftpClient.retrieveFileStream(fileName);
+
+           closeServer();
+
+           return inputStream;
+
+       } catch (IOException e) {
+           throw new FileProcessException("Error when saving file", e);
+       }
     }
 
     @Override
     public void deleteFile(String fileName) {
+        try {
+            connectServer();
 
+            ftpClient.deleteFile(fileName);
+
+            closeServer();
+        } catch (IOException e) {
+            throw new FileProcessException("Error when deleting file", e);
+        }
     }
 }
