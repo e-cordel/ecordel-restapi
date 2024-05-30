5e855d5bf6b0ff64b22b87168f071bbd3382809a
Mario Sousa <m_santos_sousa@hotmail.com>
2024-05-30 13:10:16 +0100

    Upgrade jjwt

--- src/main/java/br/com/itsmemario/ecordel/security/AuthenticationService.java
+++ src/main/java/br/com/itsmemario/ecordel/security/AuthenticationService.java
@@ -19,8 +19,11 @@ package br.com.itsmemario.ecordel.security;
 
 import br.com.itsmemario.ecordel.security.jwt.JwtToken;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +33,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 import java.util.Optional;
 
@@ -39,12 +44,12 @@ public class AuthenticationService implements UserDetailsService {
 
 	public static final String BEARER = "Bearer";
 	public static final String ROLES = "roles";
-
 	private static final String ISSUER = "e-cordel";
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private final UserRepository repository;
-	private final JwtToken jwtToken;
+	private final JwtToken jwtConfig;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -60,15 +65,18 @@ public class AuthenticationService implements UserDetailsService {
 		CordelUser principal = (CordelUser) authentication.getPrincipal();
 		
 		Date today = new Date();
-		Date expiration = new Date(today.getTime() + jwtToken.getExpiration());
-		
+		Date expiration = new Date(today.getTime() + jwtConfig.getExpiration());
+
+		byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.getSecretKey());
+		Key key = Keys.hmacShaKeyFor(keyBytes);
+
 		String token = Jwts.builder()
-			.setIssuer(ISSUER)
-			.setSubject(principal.getId().toString())
+			.issuer(ISSUER)
+			.subject(principal.getId().toString())
 			.claim(ROLES, principal.getAuthorityNames())
-			.setIssuedAt(today)
-			.setExpiration(expiration)
-			.signWith(SignatureAlgorithm.HS256, jwtToken.getSecretKey())
+			.issuedAt(today)
+			.expiration(expiration)
+			.signWith(key)
 			.compact();
 
 		return new TokenDto(token, BEARER, expiration.getTime());
@@ -76,25 +84,33 @@ public class AuthenticationService implements UserDetailsService {
 
 	public boolean isValidToken(String token) {
 		try {
-			return Jwts.parser()
-					.setSigningKey(jwtToken.getSecretKey())
-					.requireIssuer(ISSUER)
-					.parseClaimsJws(token)!=null;
-		}catch(io.jsonwebtoken.SignatureException e) {
+			return parseToken(token) != null;
+		} catch (SignatureException e) {
 			return false;
 		}
 	}
 
 	public Optional<CordelUser> getUserFromToken(String token) {
 		try {
-			Claims body = Jwts.parser().setSigningKey(jwtToken.getSecretKey()).parseClaimsJws(token).getBody();
+			Claims body = parseToken(token).getPayload();
 			Long id = Long.parseLong(body.getSubject());
 			return repository.findById(id);
-		} catch (io.jsonwebtoken.SignatureException e) {
-			logger.warn("No possible to parser token: {}", e.getMessage());
+		} catch (SignatureException e) {
+			logger.error("No possible to parser token: {}", e.getMessage());
 			return Optional.empty();
 		}
 	}
-	
-	
+
+	private Jws<Claims> parseToken(String token) throws SignatureException {
+		byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.getSecretKey());
+		SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+
+		return Jwts.parser()
+				.verifyWith(key)
+				.requireIssuer(ISSUER)
+				.build()
+				.parseSignedClaims(token);
+	}
+
+
 }
