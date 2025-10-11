cd98e3390ca2d0c3d941570d44f0b5175a68bb2c
Karlos Kelvin <karloskelvinsantos@gmail.com>
2025-10-11 12:07:22 -0300

    fix: rollback references Copyryght

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-present the original author or authors.
+ * Copyright 2021 Projeto e-cordel (http://ecordel.com.br)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 import java.net.*;
 import java.io.*;
